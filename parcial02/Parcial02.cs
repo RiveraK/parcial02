6cc444b25a4b517050bd524ea9ca5b24db52ef8c
RiveraK <48306763+RiveraK@users.noreply.github.com>
2019-03-19 19:59:19 -0600

    Add files via upload

---
+++ parcial02/Parcial02.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrarEjercicio
+{
+    public class Parcial2
+    {
+        public double num1;
+        public double num2;
+        public string nom;
+        public double mul;
+
+
+        public void Multi()
+        {
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Ingrese el segundo numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+
+            mul = num1 * num2;
+
+            Console.WriteLine("El resultado de la multiplicacion de los numero ingresados es: " +mul);
+
+
+            Console.WriteLine("El producto de 0 por cualquier numero es 0 ");
+
+
+        }
+
+
+
+        public void Division()
+        {
+            Console.WriteLine("Ingrese el primero Numero:  ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el primero Numero:  ");
+            num1 = double.Parse(Console.ReadLine());
+
+
+
+
+
+            Console.WriteLine("ERROR: No se puede dividir entre cero");
+
+        }
+        public void Datos()
+        {
+
+            for (i < 0; i++)
+            {
+                Console.WriteLine("Ingre el nombre: ");
+                nom = Console.ReadLine();
+
+
+            }
+        
+        }
+
+    }
+  }
